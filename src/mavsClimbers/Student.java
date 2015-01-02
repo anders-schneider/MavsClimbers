2326a3a7b38b249823d4ffae2e096d2743eb9652
anders-schneider <aschn@seas.upenn.edu>
2015-01-02 14:30:36 -0300

    Removed extraneous parts of Student class

--- src/mavsClimbers/Student.java
+++ src/mavsClimbers/Student.java
@@ -1,29 +1,11 @@
 package mavsClimbers;
 
-import java.util.HashMap;
-
 public class Student {
 	
 	private String name;
-	private HashMap mavNoms;
 	
 	public Student(String name) {
 		this.name = name;
-		mavNoms = new HashMap();
-	}
-	
-	public Student(String name, Teacher teacher, int rank) {
-		this.name = name;
-		mavNoms = new HashMap();
-		mavNoms.put(teacher, rank);
-	}
-	
-	public void addAsMavNom(Teacher teacher, int rank) {
-		mavNoms.put(teacher, rank);
-	}
-	
-	public int getScore(Teacher teacher) {
-		return (int) mavNoms.get(teacher);
 	}
 	
 	@Override
